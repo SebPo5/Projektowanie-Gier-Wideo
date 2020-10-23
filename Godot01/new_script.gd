19b3e40b32448de9e17453fcb6c765e18dc5fe05
SebPo5 <sebastian.porzuczek@gmail.com>
2020-10-23 11:07:55 +0200

    Pierwsze zajęcia
    Pierwsze zajęcia (Godot)

---
+++ Godot01/new_script.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var sayHallo = "Hallo hello"
+
+func _ready():
+#	print("Hello world")
+#	print(2+7)
+#	print("2+7")
+#	print("2" + "7")
+	print(sayHallo)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass

62e524f0190c4bf69e313e60f0b88950dd1216c2
SebPo5 <sebastian.porzuczek@gmail.com>
2020-10-23 12:58:03 +0200

    Pierwsze zajęcia (Godot)
    Godot – pierwsze zajęcia

--- Godot01/Node.gd
+++ Godot01/Node.gd
@@ -1,11 +1,23 @@
 extends Node
 
-# Called when the node enters the scene tree for the first time.
+var welcomeFirst = "Hi"
+var welcomeSecond = "Bonjour"
+var nameFirst = "Sebastian"
+var nameSecond = "Susan"
+var dada = 2
+
+var names = ["Sebastian", "Susan", "Ian", "Wojna Żabiomysia"]
+var welcomes = ["Czesc", "Hello", "Hi", "Bonjour"]
+var activities = ["Batrachomyomachia"]
+
 func _ready():
-	print("Hello world")
-	print("This is my first ... second line of code ever")
-
-
+# print("Hello World")
+# print (2+7)
+# print ("2+7")
+	print (welcomes[1] + " my name is " + names[1])
+	print (welcomes[2] + " my name is " + names[2])
+	print (names[3] + " zwie się " + activities[0])
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass

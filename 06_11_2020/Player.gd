d52f21f5aebd9c67eabd2a7003ed78071669a1be
SebPo5 <sebastian.porzuczek@gmail.com>
2020-11-09 18:50:36 +0100

    Commit po zajęciach (9.11)

--- 06_11_2020/Player.gd
+++ 06_11_2020/Player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400
 var screen_size
 
@@ -33,3 +35,14 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body):
+	hide()  # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false

9b642ba4ca5bb194d5ef0d16bf67dc66747cb544
SebPo5 <sebastian.porzuczek@gmail.com>
2020-12-04 11:27:46 +0100

    pw

--- pw/ball.gd
+++ pw/ball.gd
@@ -1,16 +1,10 @@
-extends Spatial
+extends RigidBody
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _process(delta):
+	var bodies = get_colliding_bodies()
+	print(bodies)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+

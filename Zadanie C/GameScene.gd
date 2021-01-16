e20350ade1d8ba754a24e5816fab2c7b1534bd37
SebPo5 <sebastian.porzuczek@gmail.com>
2021-01-16 22:23:09 +0100

    Zadanie wykonane (wszystko działa poprawnie)

--- Zadanie C/GameScene.gd
+++ Zadanie C/GameScene.gd
@@ -1,32 +1,30 @@
 extends Control
 
 var rng = RandomNumberGenerator.new()
-var test = "Twoja ostatnia liczba"
+var tries = 10
 
 func _ready():
-	$VBoxContainer/MainText.text = test
 	
 	rng.randomize()
 	var my_random_number = rng.randi_range(0, 100)
-	var number_txt = str(my_random_number)
+	get_node("SpinBoxTool").value = my_random_number
 	print(my_random_number)
-	print(number_txt)
-
-func _on_LineEdit_text_entered(new_text):
-	$VBoxContainer/MainText.text = new_text
-	if new_text =="7": print("TAK jest równy ha!")
-	#jak wyczyścić tekst po enterze? [jakiś "clear"?]
-	#załadowąć po prostu scenę zwycięstwa/porażki
 	
-	#może o to chodzi właśnie – że text=/=liczba, czyli:
-	#text–>liczba, jeśli liczba=wygenerowana..., load scene...
-#	var liczba = 0
+	get_node("SpinBoxTries").value = tries
 
-#	moja proba: if liczba==RandomNumberGenerator.new(): print ("TAK!")
-	#jeśli liczba jest większa, to zrób to...
-	#jeśli jest mniejsza, to zrób to...
-	#jeśli jest równa, to zrób to...
 #	if liczba==2: print("dwa")
 #	elif  liczba==1: print("jeden!")
 #	elif liczba ==10: print("dziesięć!")
 #	else: print("inna")
+
+func _on_SpinBoxGuess_value_changed(try):
+	tries = tries - 1
+	get_node("SpinBoxTries").value = tries
+	if try == $SpinBoxTool.value:
+		get_tree().change_scene("res://WinScene.tscn")
+	if try > $SpinBoxTool.value:
+		get_node("Label").text = "too high!"
+	if try < $SpinBoxTool.value:
+		get_node("Label").text = "too low!"
+	if tries == 0:
+		get_tree().change_scene("res://LoseScene.tscn")

8832da57dcd506bb00b4d25903f961f249f628b5
SebPo5 <sebastian.porzuczek@gmail.com>
2021-01-16 01:40:10 +0100

    Prac ciąg dalszy

--- Zadanie C/ButtonPlayAgain.gd
+++ Zadanie C/ButtonPlayAgain.gd
@@ -1,10 +1,8 @@
 extends Button
 
-
 func _ready():
 	pass # Replace with function body.
 
 
-
-func _on_ButtonPlay_pressed():
+func _on_ButtonPlayAgain_pressed():
 	get_tree().change_scene("res://GameScene.tscn")
--- Zadanie C/GameScene.gd
+++ Zadanie C/GameScene.gd
@@ -1,28 +1,30 @@
 extends Control
 
 var rng = RandomNumberGenerator.new()
-var mojTest = "test mojego tekstu"
+var test = "Twoja ostatnia liczba"
 
 func _ready():
-	#Pierwsza opcja:
-	#get_node("Label").text = "Ala ma kota"
-	#Druga opcja:
-	#$Label.text = "Kot ma na imię Blekot"
-	$VBoxContainer/TekstGlowny.text = mojTest
+	$VBoxContainer/MainText.text = test
 	
 	rng.randomize()
-	var my_random_number = rng.randf_range(0, 100)
+	var my_random_number = rng.randi_range(0, 100)
 	var number_txt = str(my_random_number)
 	print(my_random_number)
 	print(number_txt)
 
 func _on_LineEdit_text_entered(new_text):
-	$VBoxContainer/TekstGlowny.text = new_text
-	#if new_text == "7": print("TAK jest równy ha!")
+	$VBoxContainer/MainText.text = new_text
+	if new_text =="7": print("TAK jest równy ha!")
 	#jak wyczyścić tekst po enterze? [jakiś "clear"?]
+	#załadowąć po prostu scenę zwycięstwa/porażki
 	
-	#var liczba = 0
-	
-	#if liczba==2: print("dwa")
-	#elif  liczba==1: print("jeden!")
-	#else: print("inna")
+#	var liczba = 0
+
+#	moja proba: if liczba==RandomNumberGenerator.new(): print ("TAK!")
+	#jeśli liczba jest większa, to zrób to...
+	#jeśli jest mniejsza, to zrób to...
+	#jeśli jest równa, to zrób to...
+#	if liczba==2: print("dwa")
+#	elif  liczba==1: print("jeden!")
+#	elif liczba ==10: print("dziesięć!")
+#	else: print("inna")

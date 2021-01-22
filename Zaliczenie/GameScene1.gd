4179847d06df580cdbb5cb96bb8c05c88105227a
SebPo5 <sebastian.porzuczek@gmail.com>
2021-01-22 21:58:31 +0100

    Add global.gd

---
+++ Zaliczenie/GameScene1.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _ready():
+	$VBoxContainer/Name.text = global.yourName
--- Zaliczenie/StartScene.gd
+++ Zaliczenie/StartScene.gd
@@ -3,6 +3,7 @@ extends Control
 var scene_path_to_load
 
 func _ready():
+	$VBoxContainer/Name.text = "Your name is:" + global.yourName
 	$Button/ButtonPlay.grab_focus()
 	for button in $Button.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
@@ -12,5 +13,7 @@ func _on_Button_pressed(scene_to_load):
 
 func _on_LineEdit_text_entered(new_text):
 	$VBoxContainer/Name.text = new_text
+	global.yourName = new_text
+	
 	if new_text.length() > 0:
 		$VBoxContainer/LineEdit.clear()

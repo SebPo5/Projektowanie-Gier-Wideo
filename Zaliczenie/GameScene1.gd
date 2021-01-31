7ece9ea5f4b321e49ebba87d8666158d68ccc2f9
SebPo5 <sebastian.porzuczek@gmail.com>
2021-02-01 00:59:53 +0100

    lvle gotowe

--- Zaliczenie/GameScene1.gd
+++ Zaliczenie/GameScene1.gd
@@ -2,8 +2,7 @@ extends Control
 
 
 func _ready():
-	$Player/VBoxContainer/Name.text = global.yourName
-
+	$Name/PlayerName.text = global.yourName
 
 func _on_Player_endGame():
 	get_tree().change_scene("res://GameScene2.tscn")
--- Zaliczenie/GameScene2.gd
+++ Zaliczenie/GameScene2.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _ready():
-	$Player/VBoxContainer/Name.text = global.yourName
+	$Name/PlayerName.text = global.yourName
 
 func new_game():
 	$Player.start($StartPosition.position)

48df8741f379001bac42064d782f2b4c9db01697
SebPo5 <sebastian.porzuczek@gmail.com>
2021-02-01 16:05:03 +0100

    Zadanie ukończone

--- Zaliczenie/GameScene1.gd
+++ Zaliczenie/GameScene1.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _ready():
 	$Name/PlayerName.text = global.yourName
-	$HUD.show_message("Fall off the platform = game over. Enemy hits you = the level starts again. You can kill enemies by jumping on them.")
+	$HUD.show_message("Fall off the platform = game over. Nuclear mutant hits you = level restart. Kill the nuclear mutants by jumping on them.")
 
 func _on_Player_endGame():
 	get_tree().change_scene("res://GameScene2.tscn")
--- Zaliczenie/GameScene2.gd
+++ Zaliczenie/GameScene2.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready():
 	$Name/PlayerName.text = global.yourName
-	$HUD.show_message("Fall off the platform = game over. Jump on the platforms to escape the reactor!")
+	$HUD.show_message("Fall off the platform = game over.     Jump on platforms to escape the reactor!")
 	
 func new_game():
 	$Player.start($StartPosition.position)
